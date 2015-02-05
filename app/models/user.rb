629edd6b2122caa0380f46b69002fc3e2c10c02e
Tyler Pottle <tyler.pottle@gmail.com>
2015-02-05 14:17:44 -0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+  has_many :microposts, dependent: :destroy
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -68,6 +69,11 @@ class User < ActiveRecord::Base
   # Returns true if a password reset has expired
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
